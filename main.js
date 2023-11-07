374a6d68917a713c63e06160ba5fe4ef436d5572
Ralf <suess.ralf@posteo.de>
2023-11-07 08:44:35 +0100

    fixed some ide suggestions

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 60,
-  autoplay: {
-    delay: 4000,
-  },
-  navigation: {
-    nextEl: ".swiper-btn-next",
-    prevEl: ".swiper-btn-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-var swiper = new Swiper(".my-Swiper", {
+const swiper = new Swiper(".my-Swiper", {
   slidesPerView: 1,
   spaceBetween: 60,
   simulateTouch: false,
@@ -32,7 +16,7 @@ var swiper = new Swiper(".my-Swiper", {
 });
 
 // cards slider
-var swiper = new Swiper(".mySwiper2", {
+const swiper2 = new Swiper(".mySwiper2", {
   spaceBetween: 30,
   autoplay: {
     delay: 4000,
@@ -45,11 +29,11 @@ var swiper = new Swiper(".mySwiper2", {
 });
 
 // =============== infotext accordion ===============
-// easiest way to show all of the boxes, is to load the function initially, because working with css only did not work
+// easiest way to show all the boxes, is to load the function initially, because working with css only did not work
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  const accoddion = document.querySelectorAll(".accordion-btn");
-  accoddion.forEach((cell) => {
+  const accordion = document.querySelectorAll(".accordion-btn");
+  accordion.forEach((cell) => {
     let accordionContent = cell.nextElementSibling;
     accordionContent.classList.toggle('active')
     if (accordionContent.classList.contains('active')) {
@@ -61,13 +45,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 // =============== faq accordion ===============
-const accoddion2 = document.querySelectorAll(".accordion-btn2");
-accoddion2.forEach((cell) => {
+const accordion2 = document.querySelectorAll(".accordion-btn2");
+accordion2.forEach((cell) => {
   cell.addEventListener("click", () => {
     let accordionContent = cell.nextElementSibling;
 
     let icon = cell.querySelector(".svg")
-    if (icon.textContent == '+') {
+    if (icon.textContent === '+') {
       icon.textContent = '-'
     } else {
       icon.textContent = '+'
